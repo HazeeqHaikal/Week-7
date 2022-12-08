db617614071d87798b716a515e31647e288225dc
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-12-08 19:39:37 +0800

    update assignment

--- monthly income/income.cpp
+++ monthly income/income.cpp
@@ -9,35 +9,42 @@ int main() {
     float commision;
     string name;
     char itemType;
+    int workers;
 
-    cout << "Enter your name: ";
-    getline(cin, name);
-    cout << "A (TV)\nB (HiFi Set)\nC (Microwave)\nD (Washing Machine)\nE (Refrigerator)\n";
-    cout << "Enter the item letter: ";
-    cin >> itemType;
-    itemType = toupper(itemType);
-    switch (itemType) {
-        case 'A':
-            commision = 0.05;
-            break;
-        case 'B':
-            commision = 0.08;
-            break;
-        case 'C':
-            commision = 0.10;
-            break;
-        case 'D':
-            commision = 0.13;
-            break;
-        case 'E':
-            commision = 0.15;
-            break;
-        default:
-            cout << "Invalid item type." << endl;
-            return 0;
+    cout << "Enter how many workers you have: ";
+    cin >> workers;
+
+    for (int i = 0; i < workers; i++) {
+        cin.ignore();
+        cout << "\nEnter your name: ";
+        getline(cin, name);
+        cout << "A (TV)\nB (HiFi Set)\nC (Microwave)\nD (Washing Machine)\nE (Refrigerator)\n";
+        cout << "Enter the item letter: ";
+        cin >> itemType;
+        itemType = toupper(itemType);
+        switch (itemType) {
+            case 'A':
+                commision = 0.05;
+                break;
+            case 'B':
+                commision = 0.08;
+                break;
+            case 'C':
+                commision = 0.10;
+                break;
+            case 'D':
+                commision = 0.13;
+                break;
+            case 'E':
+                commision = 0.15;
+                break;
+            default:
+                cout << "Invalid item type." << endl;
+                return 0;
+                cin.ignore();
+        }
+        cout << fixed << setprecision(2) << "\nMonthly income for " << name << " is RM" << monthlySalary + (monthlySalary * (float)commision) << endl;
     }
-
-    cout << fixed << setprecision(2) << "\nMonthly income for " << name << " is RM" << monthlySalary + (monthlySalary * (float)commision) << endl;
 
     return 0;
 }
